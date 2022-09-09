7ee1b7e6608ac5125246ddcf7c9cb69cb5c09d07
aungmyatmoe2021 <aungmyatmoe2021@gmail.com>
2022-09-09 11:46:58 +0800

    added version number

--- routes/default_router.js
+++ routes/default_router.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res) => {
-    return res.status(200).send("<h1>Welcome from Myanmar Culture Information</h1>")
+router.get('/', (req, res) => {
+    return res.status(200).send("<h1>Welcome from Myanmar Culture Information Version 1.0.0</h1>")
 })
 
 module.exports = router;
